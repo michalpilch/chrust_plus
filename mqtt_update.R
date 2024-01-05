c683af80a0121b02f039b473bca2ef19edbdff7a
michal_pilch <mikhael.pilch@gmail.com>
2024-01-05 17:05:01 +0100

    automatic update

--- mqtt_update.R
+++ mqtt_update.R
@@ -4,6 +4,8 @@ require(feather)
 #dump data
 system(command = "rsync -abviuzP pi@192.168.0.139:~/domoticz/domoticz.db /home/dsl/Dokumenty/R/mqtt/chrust_plus/")
 # Create an ephemeral in-memory RSQLite database
+#system(command = "rsync -abviuzP /home/dsl/Dokumenty/R/mqtt/chrust_plus/domoticz.db~ pi@192.168.0.139:~/domoticz/")
+# 
 #con <- dbConnect(RSQLite::SQLite(), "/home/dsl/Dokumenty/R/mqtt/domoticz.db")
 con <- dbConnect(RSQLite::SQLite(), "domoticz.db")
 #  read temperature data
